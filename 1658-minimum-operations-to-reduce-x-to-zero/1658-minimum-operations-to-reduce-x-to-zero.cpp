47e121bb8641c4f2fc6474bb4ba7402931f292d5
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-02-13 00:55:42 +0530

    Time: 384 ms (40.15%), Space: 166.7 MB (19.50%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    long long max(long long a, long long b) {
+    int max(int a, int b) {
         return a>b ? a :b;
     }
     int minOperations(vector<int>& nums, int x) {
         unordered_map<long long,int>mp;
-       long long int s=0,maxi=LONG_MIN;
+       int s=0,maxi=INT_MIN;
         int n=nums.size();
         for(int i :nums) s+=i;
         s-=x; if(s==0) return n;
         mp[0]=-1;
         int sum=0;
         for(int i=0;i<nums.size();i++) {
-            sum+=nums[i]; //cout<<sum<<" ";
-            if(mp.count(sum-s)) { cout<<1;
+            sum+=nums[i]; 
+            if(mp.count(sum-s)) {
                 maxi=max(maxi,i-mp[sum-s]);
             }
             mp[sum]=i;
         }
-        return maxi==LONG_MIN?-1:n-maxi;
+        return maxi==INT_MIN?-1:n-maxi;
     }
 };
