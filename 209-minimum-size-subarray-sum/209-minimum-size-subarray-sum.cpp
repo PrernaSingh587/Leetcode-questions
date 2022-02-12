9d77e34d30b1637dc6b60ce3dae42080176cb017
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-02-13 00:26:32 +0530

    Time: 12 ms (45.32%), Space: 10.7 MB (14.89%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int minSubArrayLen(int t, vector<int>& nums) {
-        int mini=INT_MAX;
-        for(int i=0;i<nums.size();i++) { int s=0;
-            for(int j=i;j<nums.size();j++) {
-                s+=nums[j];
-                if(s>=t) {
-                    mini=min(mini,j-i+1); break;
-                }
-            }
+        int ans=INT_MAX;
+        int dp[nums.size()]; 
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++) dp[i]=dp[i-1]+nums[i];
+        for(int i=0;i<nums.size();i++) { 
+             int l=i,r=nums.size()-1;
+             while(l<=r) {
+                 int m=l+(r-l)/2;
+                 if(i==0) {
+                     if(dp[m]>=t) {
+                         ans=min(ans,m+1);
+                         r=m-1;
+                     } else {
+                         l=m+1;
+                     }
+                 } else {
+                     if((dp[m]-dp[i-1])>=t) {
+                          ans=min(ans,m-i+1);
+                         r=m-1;
+                     } else l=m+1;
+                 }
+             }
+            // for(int j=i;j<nums.size();j++) {
+            //     s+=nums[j];
+            //     if(s>=t) {
+            //         mini=min(mini,j-i+1); break;
+            //     }
+            // }
         }
-        return mini==INT_MAX?0:mini;
+        return ans==INT_MAX?0:ans;
     }
 };
