b373a7f3c8ec2901db99f2d2e3e0f9fa30959bf6
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-03-16 22:45:06 +0530

    Time: 229 ms (48.35%), Space: 9.6 MB (71.16%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.cpp
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -3,29 +3,21 @@
 
 class Solution {
 public:
-    int findCelebrity(int n) {
-        
-        int ind[n],cnd[n]; memset(ind,0,sizeof(ind));
-        memset(cnd,0,sizeof(cnd));
-        int ans=-1; //,f=0;
-        for(int i=0;i<n;i++) {
-            ans=i;
-            for(int j=0;j<n;j++) {
-                if(i==j) continue;
-                if(knows(i,j)) {
-                   ans=-1; break;
-                }
-            } 
-           
-            if(ans==i) break;
-        }
-        if(ans==-1) return -1;
-       // cout<<1;
-        int c=0;
+    bool isCeleb(int ans,int n) {
         for(int i=0;i<n;i++) {
             if(i==ans) continue;
-            if(knows(i,ans)) c++;
+            if(knows(i,ans) && !knows(ans,i)) continue;
+            return 0;
         }
-        return c==n-1?ans:-1;
+        return 1;
     }
+    int findCelebrity(int n) {
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            if(knows(ans,i)) ans=i;
+        }
+        if(isCeleb(ans,n)) return ans;
+        return -1;
+    }
+    
 };
