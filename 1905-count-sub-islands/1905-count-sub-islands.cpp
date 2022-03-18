1290220da9de1fa1372cc0bc1a1edd81ba4f1858
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-03-19 04:44:35 +0530

    Time: 1072 ms (5.02%), Space: 253.7 MB (5.02%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int countSubIslands(vector<vector<int>>& g1, vector<vector<int>>& g2) {
-        for(int i=0;i<g1.size();i++) {
-            for(int j=0;j<g1[i].size();j++) {
-                g1[i][j]+=g2[i][j];
-            }
-        }
+        
         int dx[]={0,0,-1,1},dy[]={-1,1,0,0};
         int n=g1.size(),m=g1[0].size();
         int vis[n][m]; memset(vis,0,sizeof(vis));
         int ans=0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(!vis[i][j] && g1[i][j]==2) {
+                if(!vis[i][j] && g2[i][j]==1 && g1[i][j]) {
                     queue<vector<int>>q;
                     q.push({i,j});
                     vis[i][j]=1;
@@ -26,11 +22,11 @@ public:
                                 if(!(X>=0 && Y>=0 && X<n && Y<m) || 
                                   vis[X][Y]) continue;
                                    if(g2[X][Y]==1 
-                                   && g1[X][Y]==2) {
+                                   && g1[X][Y]==1) {
                                     vis[X][Y]=1;
                                     q.push({X,Y});
                                 } else if(g2[X][Y]==1 
-                                   && g1[X][Y]!=2) {
+                                   && g1[X][Y]!=1) {
                                        ff=1; vis[X][Y]=1;
                                     q.push({X,Y});
                                    }
