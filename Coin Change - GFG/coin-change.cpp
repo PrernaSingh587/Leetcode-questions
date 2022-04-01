9921f25cd21815b6d1c9b5bac8255509dbc8e4fd
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-04-01 23:35:44 +0530

    Added solution - LeetHub

---
+++ Coin Change - GFG/coin-change.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    long long int count(int S[], int m, int n) {
+        long long int dp[m+1][n+1]; memset(dp,0,sizeof(dp));
+        for(int i=0;i<=m;i++) dp[i][0]=1;
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(S[i-1]>j) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=dp[i-1][j]+dp[i][j-S[i-1]];
+            }
+        }
+        return dp[m][n];
+       
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int arr[m];
+        for (int i = 0; i < m; i++) cin >> arr[i];
+        Solution ob;
+        cout << ob.count(arr, m, n) << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
