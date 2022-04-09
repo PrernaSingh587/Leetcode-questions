cf9ae07ebd4b52b08d9c92b3405ad6cb363d7e82
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-04-09 22:23:03 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        { vector <int>v;
+            int i=0,j=0,k=0;
+            while(i<n1 && j<n2 && k<n3) {
+                if(A[i]==B[j] && B[j]==C[k]) {
+                    v.push_back(B[j]);
+                    i++;
+                    while(i<n1 && A[i-1]==A[i]) i++;
+                    j++;
+                    k++;
+                    while(j<n2 && B[j-1]==B[j]) j++;
+                    while(k<n3 && C[k-1]==C[k]) k++;
+                } else {
+                    int ind=-1,f;
+                    int mini=INT_MAX;
+                    if(mini>A[i]) {
+                        f=0;
+                        mini=A[i]; 
+                    }
+                    if(mini>B[j]) {
+                        f=1;
+                        mini=B[j]; 
+                    }
+                    if(mini>C[k]) {
+                        f=3;
+                        mini=C[k]; 
+                    }
+                    if(f==0) i++;
+                    if(f==1) j++;
+                    if(f==3) k++;
+                }
+                
+            }
+            return v;
+        }
+
+};
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}  // } Driver Code Ends
