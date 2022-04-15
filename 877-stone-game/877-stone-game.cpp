aee329a1959011fe3f48a6c55db4d934e880957f
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-04-15 14:38:50 +0530

    Time: 657 ms (5.03%), Space: 69.4 MB (5.03%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int vis[501][501];
-    int solve(vector<int>p, int i,int j) {
-        if(i==j) return p[i];
-        if(vis[i][j]!=-1) return vis[i][j];
-         return vis[i][j]=max(p[i]-solve(p,i+1,j),p[j]-solve(p,i,j-1));
+    map<pair<int,int>,int>mp;
+    int solve(vector<int>& p,int i,int j) {
+        if(i>j) return 0;
+        if(mp.count({i,j})) return mp[{i,j}];
+        return mp[{i,j}]=max(p[i]-solve(p,i+1,j),p[j]-solve(p,i,j-1));
     }
     bool stoneGame(vector<int>& p) {
-        memset(vis,-1,sizeof(vis));
-        int h=solve(p,0,p.size()-1);
+        int i=0,j=p.size()-1;
+        int h=solve(p,i,j);
         return h>0;
     }
 };
