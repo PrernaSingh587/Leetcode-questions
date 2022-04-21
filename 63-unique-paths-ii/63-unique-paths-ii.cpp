752ab19d5b6ea4a2e48ccbd93aff3b3c568434d6
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-04-21 23:06:54 +0530

    Time: 4 ms (63.63%), Space: 7.7 MB (72.79%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int h = obstacleGrid.size();
-    if(h == 0) return 0;
-    int w = obstacleGrid[0].size();
-    if(w == 0) return 0;
-    if(obstacleGrid[0][0]) return 0;
-    
-    // first cell has 1 path
-    obstacleGrid[0][0] = 1;
-    
-    // first row all are '1' until obstacle (from left only)
-    for(int i=1; i<w; i++){
-        obstacleGrid[0][i] = obstacleGrid[0][i] ? 0 : obstacleGrid[0][i-1];
-    }
-
-    for(int j=1; j<h; j++){
-        // first column is like first row (from top only)
-        obstacleGrid[j][0] = obstacleGrid[j][0] ? 0 : obstacleGrid[j-1][0];
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
         
-        // others are up+left
-        for(int i=1; i<w; i++){
-            obstacleGrid[j][i] = obstacleGrid[j][i] ? 0 : obstacleGrid[j-1][i] + obstacleGrid[j][i-1];
+         int m=g.size(),n=g[0].size();
+        if(m==0 || n==0) return 0;
+        if(g[m-1][n-1] || g[0][0]) return 0;
+        g[0][0]=1;
+        for(int i=1;i<m;i++) {
+            if(g[i-1][0]==0 ||g[i][0]==1 ) g[i][0]=0;
+            else g[i][0]=1;
         }
-    }
-    
-    return obstacleGrid[h-1][w-1];
+        for(int i=1;i<n;i++) {
+            if(g[0][i-1]==0 || g[0][i]==1) g[0][i]=0;
+            else g[0][i]=1;
+        }
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) {
+                if(g[i][j]==1) {
+                    g[i][j]=0;
+                } else {
+                    g[i][j]=(g[i-1][j]+g[i][j-1]);
+                }
+            }
+        }
+       
+        return g[m-1][n-1];
     }
 };
