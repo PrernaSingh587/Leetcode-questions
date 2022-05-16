c7214eaa82d6ad7dacafb078a0461ac75594acf7
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-05-16 21:16:48 +0530

    Time: 3 ms (90.89%), Space: 17.3 MB (8.00%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,27 +11,29 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& p,int st,int en) {
-        if(st>en) return NULL;
-        if(st==en) {
-           return new TreeNode(p[st]);
-        }
-        int i=st;
-        while(i<=en && p[st]>=p[i]) {
-            i++;
-        }
-        TreeNode*root = new TreeNode(p[st]);
+    TreeNode*solve(vector<int>v,int i,int j) {
+     if(i>j) return NULL;
+     if(i==j) return new TreeNode(v[i]);
+     TreeNode* head=new TreeNode(v[i]);
+     int x=i+1;
         
-            TreeNode *l=solve(p,st+1,i-1);
-            TreeNode *r=solve(p,i,en);
-            root->left=l;
-            root->right=r;
-            return root;
-      
-        
-       
-    }
-    TreeNode* bstFromPreorder(vector<int>& p) {
-        return solve(p,0,p.size()-1);
+     while(x<=j && v[x]<v[i]) {x++;}
+        if(x>(i+1)) x--;
+     TreeNode*left=NULL,*right=NULL;
+        //cout<<v[i]<<" "<<v[x]<<endl;
+     if(v[i]>v[i+1]) 
+     {left=solve(v,i+1,x);
+     right=solve(v,x+1,j);} else {
+         right=solve(v,x,j);
+     }
+     head->left=left;
+     head->right=right;
+    // cout<<head->val<<" "<<head->left->val<<" ";
+     return head;
+ }
+
+    TreeNode* bstFromPreorder(vector<int> &v) {
+         int h=v.size()-1;
+    return solve(v,0,h);
     }
 };
