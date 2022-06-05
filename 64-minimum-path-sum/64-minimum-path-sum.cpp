4796759ab1a566358101e4e7f25678455c67d76f
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-06-05 20:56:36 +0530

    Time: 21 ms (14.97%), Space: 9.5 MB (99.93%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& g) {
-        int m=g.size(),n=g[0].size();
-       // int dp[m][n]; memset(dp,0,sizeof(dp));
-        for(int i=m-2;i>=0;i--) {
-            g[i][n-1]+=g[i+1][n-1];
-        }
+        int n=g.size(),m=g[0].size();
         for(int i=n-2;i>=0;i--) {
-             g[m-1][i]+=g[m-1][i+1];
+            g[i][m-1]+=g[i+1][m-1];
         }
+        for(int j=m-2;j>=0;j--) g[n-1][j]+=g[n-1][j+1];
         
-        for(int i=m-2;i>=0;i--) {
-            for(int j=n-2;j>=0;j--) {
-                g[i][j]=min(g[i+1][j],g[i][j+1])+g[i][j];
-            }
+        for(int i=n-2;i>=0;i--) {
+            for(int j=m-2;j>=0;j--)
+                g[i][j]+=min(g[i+1][j],g[i][j+1]);
         }
         return g[0][0];
     }
