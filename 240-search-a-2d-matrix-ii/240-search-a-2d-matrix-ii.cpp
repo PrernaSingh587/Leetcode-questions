7648f4f5065fb34ced3dd981e0a0598fe49fb356
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-07-27 19:36:53 +0530

    Time: 1582 ms (5.02%), Space: 14.9 MB (68.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int tar) {
+        int m=mat.size(),n=mat[0].size();
+        int i=m-1,j=0;
+        while(i>=0 && j>=0 && i<m && j<n ) {
+            cout<<mat[i][j]<<" ";
+            if(mat[i][j]>tar) {
+                i--;
+            } else if(mat[i][j]<tar) j++;
+            else return 1;
+        }
+        return 0;
+    }
+};
