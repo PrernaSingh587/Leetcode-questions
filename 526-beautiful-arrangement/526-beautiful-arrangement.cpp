f015dfd338215a6ef280b72adc24a00dab031338
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-08-05 13:30:41 +0530

    Time: 98 ms (77.51%), Space: 5.8 MB (95.66%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int seen[16];
+    int seen=0;
     int dfs(int n,int pos) {
         if(pos>n) return 1;
         int res=0;
         for(int i=1;i<=n;i++) {
-            if(!seen[i] && (!(pos%i) || !(i%pos))) {
-                seen[i]=1;
+            int bit =1<<i;
+            if((seen&bit)==0 && (!(pos%i) || !(i%pos))) {
+               seen=seen^bit;
                 res += dfs(n,pos+1);
-                seen[i]=0;
+                 seen=seen^bit;
+                
             }
         }
         return res;
     }
     int countArrangement(int n) {
-        memset(seen,0,sizeof(seen));
+       
         return dfs(n,1);
     }
 };
