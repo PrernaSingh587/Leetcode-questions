72604ff81161b7e7deb3b1a604a13bcfc873ea60
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-08-08 00:11:54 +0530

    Time: 102 ms (9.00%), Space: 26.1 MB (5.01%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& g) {
         int n=g.size(),m=g[0].size();
-        //int vis[n][m]; memset(vis,0,sizeof(vis));
-        int ans=0;
-        int dx[]={0,0,1,-1},dy[]={-1,1,0,0};
+        int dx[]={-1,1,0,0},dy[]={0,0,-1,1};
+        int c=0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(g[i][j]=='1') {
+                if(g[i][j]=='1') { c++;
+                                  g[i][j]='0';
                     queue<vector<int>>q;
                     q.push({i,j});
-                    g[i][j]='0'; ans++;
                     while(!q.empty()) {
-                        int s=q.size();
-                        while(s--) {
-                            vector<int>v=q.front(); q.pop();
-                            for(int k=0;k<4;k++) {
-                                int X=v[0]+dx[k],Y=dy[k]+v[1];
-                                if(X>=0 && Y>=0 && X<n && Y<m
-                                  && g[X][Y]=='1') {
-                                    g[X][Y]='0';
-                                    q.push({X,Y});
-                                }
+                        vector<int>v = q.front();
+                        q.pop();
+                        int x=v[0],y=v[1];
+                        
+                        for(int h=0;h<4;h++) {
+                            int X=x+dx[h],Y=y+dy[h];
+                            if(X>=0 && Y>=0 && X<n && Y<m
+                              && g[X][Y]=='1') {
+                                g[X][Y]='0';
+                                q.push({X,Y});
                             }
+                                
                         }
                     }
                 }
             }
         }
-        return ans;
+        return c;
     }
 };
