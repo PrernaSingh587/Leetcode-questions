94b4d38a4d2d1e8de7555abac7ccfbcae890b82b
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-08-14 15:58:24 +0530

    Time: 19 ms (20.09%), Space: 13.6 MB (15.18%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& g) {
         int n=g.size(),m=g[0].size();
-        //int vis[n][m]; memset(vis,0,sizeof(vis));
-        int ans=0;
-        int dx[]={0,0,1,-1},dy[]={-1,1,0,0};
-         queue<vector<int>>q;
-        int fresh=0;
+        queue<vector<int>>q;
+        int a=0; //,b=0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                 if(g[i][j]==2)
-                    q.push({i,j}); 
-                else if(g[i][j]==1) fresh++;
+                if(g[i][j]==2) {q.push({i,j});}
+                if(g[i][j]==1) a++;
             }
         }
-                int fff=0;
-                    while(!q.empty()) {
-                        fff=1;
-                        int s=q.size();
-                        ans++;
-                        while(s--) {
-                            vector<int>v=q.front(); q.pop();
-                            for(int k=0;k<4;k++) {
-                                int X=v[0]+dx[k],Y=dy[k]+v[1];
-                                if(X>=0 && Y>=0 && X<n && Y<m
-                                  && g[X][Y]==1) {
-                                    g[X][Y]=0; fresh--;
-                                    q.push({X,Y});
-                                }
-                            }
-                        }
+        if(a==0) return 0;
+        int dx[]={-1,1,0,0},dy[]={0,0,-1,1};
+        int c=0;
+        while(!q.empty()) {
+           int s=q.size();
+            while(s--) {
+                vector<int>p=q.front(); q.pop();
+                int x=p[0],y=p[1];
+                for(int i=0;i<4;i++) {
+                    int X=x+dx[i],Y=y+dy[i];
+                    if(X>=0 && Y>=0 && X<n && Y<m && g[X][Y]==1) {
+                        g[X][Y]=2; a--;
+                        q.push({X,Y});
                     }
-                
-            
-        if(fff==0 && fresh==0) return 0;
-        return fresh==0?ans-1:-1;
+                }
+            }
+            c++;
+        }
+        return a==0 ? c-1 : -1;
     }
 };
