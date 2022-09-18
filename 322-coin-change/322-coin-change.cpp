a660adfea5d018b8994e23a2335ad82249338b05
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-09-18 15:50:44 +0530

    Time: 195 ms (30.02%), Space: 10.1 MB (93.02%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int coinChange(vector<int>& c, int amt) {
         int n=c.size();
-        int dp[n+1][amt+1];
+        int dp[amt+1];
         memset(dp,0,sizeof(dp));
-        for(int i=1;i<=amt;i++) dp[0][i]=INT_MAX-1;
+        for(int i=1;i<=amt;i++) dp[i]=INT_MAX;
         
-        
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=amt;j++) {
-                if(c[i-1]>j) dp[i][j]=dp[i-1][j];
-                else dp[i][j]=min(1+dp[i][j-c[i-1]],dp[i-1][j]);
+        for(int i=1;i<=amt;i++) {
+            for(int j=0;j<n;j++) {
+                if(i-c[j]>=0 && dp[i-c[j]]!=INT_MAX) {
+                    dp[i]=min(dp[i],1+dp[i-c[j]]);
+                }
             }
         }
-        return dp[n][amt]!=((INT_MAX-1))?dp[n][amt]:-1;
+        return dp[amt]==INT_MAX?-1:dp[amt];
     }
 };
