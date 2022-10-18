376cfbb9a135d633ce2960f82069c783caeec945
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-10-19 00:50:35 +0530

    Time: 507 ms (5.07%), Space: 117.3 MB (5.07%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    map<pair<int,int>,vector<int>>mp;
+    map<pair<int,int>,int>mp;
     map<pair<int,int>,int>mp1;
-    vector<int> p(int i,int j,vector<int>arr) {
-        if(i==j) return {arr[i],0};
-        if(j-i==1) return {arr[i]*arr[j],arr[i]*arr[j]};
-        int mini=INT_MAX,c=1;
+    int p(int i,int j,vector<int>arr) {
+        if(i==j) return 0;
+        if(j-i==1) return arr[i]*arr[j];
+        int mini=INT_MAX;
         if(mp.count({i,j})) return mp[{i,j}];
-        vector<int>l,r;
-        for(int k=i;k<j;k++) {
-            l=p(i,k,arr); r=p(k+1,j,arr);
-            int a=0,b=0;
-            if(mp1.count({i,k})) a=mp1[{i,k}]; else {
-                for(int x=i;x<=k;x++) a=max(a,arr[x]);
-                mp1[{i,k}]=a;
-            }
-            if(mp1.count({k+1,j})) b=mp1[{k+1,j}]; 
-            else {
-                for(int x=k+1;x<=j;x++) b=max(b,arr[x]);
-                mp1[{k+1,j}]=b;
-            }
+        priority_queue<int>q;
+        int prod=0;
+        if(mp1.count({i,j})) prod = mp1[{i,j}];
+        else {
+            for(int k=i;k<=j;k++) q.push(arr[k]);
+            prod=q.top();  //";
+            q.pop(); prod*=q.top();
+            //cout<<q.top()<<" ";
             
-            
-             //cout<<mini<<" "<<l[1]<<" "<<r[1]<<" "<<c<<endl;
-            if(mini>l[1]+r[1]+(a*b)) {
-                mini = l[1]+r[1]+(a*b);
-                c=a*b;
-               
-            }
         }
-        return mp[{i,j}]={c,mini};
+      
+        for(int k=i;k<j;k++) {
+            mini=min(mini,p(i,k,arr)+p(k+1,j,arr));
+        }
+        return mp[{i,j}]=mini+prod;
     }
     int mctFromLeafValues(vector<int>& arr) {
         int n=arr.size();
-       vector<int>ans= p(0,n-1,arr);
-        return ans[1];
+       return p(0,n-1,arr);
+      
         
     }
 };
