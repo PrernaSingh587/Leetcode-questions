05635ccf751447e50053999f6ee38dd8f9d02365
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2022-10-30 21:09:46 +0530

    Time: 16 ms (60.95%), Space: 9.6 MB (59.57%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int x=0;
-        for(int i=31;i>=0;i--) {
-            int c=0;
-            for(int j : nums) {
-                int d = (j>>i)&1;
-                c+=d;
-            }
-            int y=(c%3);
-            x+=(y<<i);
+      int ones=0,twos=0;
+        for(int i : nums) {
+            ones = (i^ones)&(~twos);
+            twos = (i^twos)&(~ones);
         }
-        return x;
+        return ones;
     }
 };
