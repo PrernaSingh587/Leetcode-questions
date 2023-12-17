d19abea76bf272de2ea8487a11b71330a8acddae
Prerna Singh <51945896+PrernaSingh587@users.noreply.github.com>
2023-12-17 13:38:44 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -1,36 +1,41 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
-    long long int count(int S[], int m, int n) {
-        long long int dp[m+1][n+1]; memset(dp,0,sizeof(dp));
-        for(int i=0;i<=m;i++) dp[i][0]=1;
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=n;j++) {
-                if(S[i-1]>j) dp[i][j]=dp[i-1][j];
-                else dp[i][j]=dp[i-1][j]+dp[i][j-S[i-1]];
+  #define ll long long
+    long long int count(int coins[], int N, int sum) {
+        ll dp[N+1][sum+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<=N;i++) dp[i][0]=1;
+        for(int i=1;i<=N;i++) {
+            for(int j=1;j<=sum;j++) {
+                if(coins[i-1]<=j) dp[i][j]=dp[i][j-coins[i-1]];
+                dp[i][j]+=dp[i-1][j];
             }
         }
-        return dp[m][n];
-       
+        return dp[N][sum];
+        
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        int arr[m];
-        for (int i = 0; i < m; i++) cin >> arr[i];
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
         Solution ob;
-        cout << ob.count(arr, m, n) << endl;
+        cout << ob.count(coins, N, sum) << endl;
     }
 
     return 0;
-}  // } Driver Code Ends
+}
+
+
+// } Driver Code Ends
